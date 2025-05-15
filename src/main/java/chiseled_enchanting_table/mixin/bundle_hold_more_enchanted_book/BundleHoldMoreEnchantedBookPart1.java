6777cd7ff8110dfc8fb9af811cf5cfab1b3907ba
Sacramentix <benji.u1225u@gmail.com>
2025-05-15 21:35:20 +0200

    bundle can hold 16 book

---
+++ src/main/java/chiseled_enchanting_table/mixin/bundle_hold_more_enchanted_book/BundleHoldMoreEnchantedBookPart1.java
@@ -0,0 +1,50 @@
+package chiseled_enchanting_table.mixin.bundle_hold_more_enchanted_book;
+
+import java.util.List;
+
+import org.apache.commons.lang3.math.Fraction;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import net.minecraft.component.type.BundleContentsComponent;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+
+@Mixin(BundleContentsComponent.class)
+public class BundleHoldMoreEnchantedBookPart1 {
+
+    @Shadow
+    private static native Fraction getOccupancy(ItemStack stack);
+
+
+    @Redirect(
+        method = "<init>(Ljava/util/List;)V",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/component/type/BundleContentsComponent;calculateOccupancy(Ljava/util/List;)Lorg/apache/commons/lang3/math/Fraction;"
+        )
+    )
+    private static Fraction onConstructor(List<ItemStack> stacks) {
+        // BundleItem BundleContentsComponent$Builder
+        return calculateOccupancyModified(stacks);
+    }
+
+    private static Fraction calculateOccupancyModified(List<ItemStack> stacks) {
+        var fraction = Fraction.ZERO;
+        for (var stack : stacks) {
+
+            var occupancy = 
+                stack.isOf(Items.ENCHANTED_BOOK) ? Fraction.getFraction(1, 16) : getOccupancy(stack);
+
+            fraction = fraction.add(
+                occupancy
+                    .multiplyBy(
+                        Fraction.getFraction(stack.getCount(), 1)
+                    )
+            );
+        }
+        return fraction;
+   }
+
+}
---
+++ src/main/java/chiseled_enchanting_table/mixin/bundle_hold_more_enchanted_book/BundleHoldMoreEnchantedBookPart2.java
@@ -0,0 +1,69 @@
+package chiseled_enchanting_table.mixin.bundle_hold_more_enchanted_book;
+
+import java.util.List;
+
+import org.apache.commons.lang3.math.Fraction;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import net.minecraft.block.entity.BeehiveBlockEntity;
+import net.minecraft.component.DataComponentTypes;
+import net.minecraft.component.type.BundleContentsComponent;
+import net.minecraft.item.BundleItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+
+
+@Mixin(targets = "net.minecraft.component.type.BundleContentsComponent$Builder")
+public class BundleHoldMoreEnchantedBookPart2 {
+    // @Shadow
+    // private static final Fraction NESTED_BUNDLE_OCCUPANCY;
+
+    @Redirect(
+        method = "add(Lnet/minecraft/item/ItemStack;)I",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/component/type/BundleContentsComponent;getOccupancy(Lnet/minecraft/item/ItemStack;)Lorg/apache/commons/lang3/math/Fraction;"
+        )
+    )
+    private static Fraction add(ItemStack stack) {
+        return alteredGetOccupancy(stack);
+    }
+
+    @Redirect(
+        method = "getMaxAllowed(Lnet/minecraft/item/ItemStack;)I",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/component/type/BundleContentsComponent;getOccupancy(Lnet/minecraft/item/ItemStack;)Lorg/apache/commons/lang3/math/Fraction;"
+        )
+    )
+    private static Fraction getMaxAllowed(ItemStack stack) {
+        return alteredGetOccupancy(stack);
+    }
+
+    @Redirect(
+        method = "removeFirst()Lnet/minecraft/item/ItemStack;",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/component/type/BundleContentsComponent;getOccupancy(Lnet/minecraft/item/ItemStack;)Lorg/apache/commons/lang3/math/Fraction;"
+        )
+    )
+    private static Fraction removeFirst(ItemStack stack) {
+        return alteredGetOccupancy(stack);
+    }
+
+
+    private static Fraction alteredGetOccupancy(ItemStack stack) {
+        BundleContentsComponent bundleContentsComponent = (BundleContentsComponent)stack.get(DataComponentTypes.BUNDLE_CONTENTS);
+        if (bundleContentsComponent != null) {
+            return Fraction.getFraction(1,16).add(bundleContentsComponent.getOccupancy());
+        } else {
+            List<BeehiveBlockEntity.BeeData> list = (List)stack.getOrDefault(DataComponentTypes.BEES, List.of());
+            return !list.isEmpty() ? Fraction.ONE : Fraction.getFraction(1, stack.isOf(Items.ENCHANTED_BOOK)? 16 : stack.getMaxCount());
+        }
+    }
+
+}
