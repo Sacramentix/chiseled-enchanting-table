8bbbd70999a77d6f64fda42b9e6715c1a4fe4462
Sacramentix <benji.u1225u@gmail.com>
2025-05-26 01:51:18 +0200

    stackable enchanted book  and no merge from anvil

--- src/main/java/chiseled_enchanting_table/ChiseledEnchantingTable.java
+++ src/main/java/chiseled_enchanting_table/ChiseledEnchantingTable.java
@@ -5,12 +5,16 @@ import org.slf4j.LoggerFactory;
 
 import chiseled_enchanting_table.modifyChestLootTable.ReworkEnchantedBookChestLoot;
 import chiseled_enchanting_table.modifyChestLootTable.abandoned_mineshaft.DepthBasedEfficiency;
+import chiseled_enchanting_table.mixin.accessor.ItemAccessor;
 import chiseled_enchanting_table.modifyChestLootTable.RemoveEnchantedBooksFunction;
 import chiseled_enchanting_table.registry.BlockRegistry;
 import chiseled_enchanting_table.registry.EntityRegistry;
 import chiseled_enchanting_table.registry.ScreenHandlerRegistry;
 import chiseled_enchanting_table.registry.StructureProcessorRegistry;
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.component.ComponentMap;
+import net.minecraft.component.DataComponentTypes;
+import net.minecraft.item.Items;
 import net.minecraft.server.SaveLoading.ServerConfig;
 import net.minecraft.util.Identifier;
 
@@ -23,6 +27,15 @@ public class ChiseledEnchantingTable implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		LOGGER.info("Chiseling the enchant system!");
+
+		((ItemAccessor) Items.ENCHANTED_BOOK)
+			.setComponents(
+				ComponentMap.of(
+					((ItemAccessor) Items.ENCHANTED_BOOK).getComponents(),
+					ComponentMap.builder().add(DataComponentTypes.MAX_STACK_SIZE, 64).build()
+				)
+			);
+
 		// SERVER_CONFIG = ConfigApiJava.registerAndLoadConfig(ServerConfig::new, RegisterType.BOTH);
 		BlockRegistry.init();
 		EntityRegistry.init();
